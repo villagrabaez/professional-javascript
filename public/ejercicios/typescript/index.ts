c0f4f7981a77417c90de7d09264b4804200f64f0
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-05-03 15:45:49 -0400

    Empezando con Typescript

---
+++ public/ejercicios/typescript/index.ts
@@ -0,0 +1,10 @@
+console.log('Hello TapyScript!');
+
+function add(a: number, b: number) {
+  let suma = a + b;
+  return suma;
+}
+
+const sum = add(2, 2);
+
+console.log(sum);
--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,47 @@
-// ServiceWorker
+// // ServiceWorker
 
-const VERSION = 'v1'
+// const VERSION = 'v1'
 
-self.addEventListener('install', event => {
-  event.waitUntil(precache());
-});
+// self.addEventListener('install', event => {
+//   event.waitUntil(precache());
+// });
 
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  // get: solo peticiones de tipo get
-  if (request.method != 'GET') {
-    return;
-  }
+// self.addEventListener('fetch', event => {
+//   const request = event.request;
+//   // get: solo peticiones de tipo get
+//   if (request.method != 'GET') {
+//     return;
+//   }
 
-  // buscar en cache
-  event.respondWith(cacheResponse(request));
+//   // buscar en cache
+//   event.respondWith(cacheResponse(request));
 
-  // actualizar cache
-  event.respondWith(updateCache(request));
-});
+//   // actualizar cache
+//   event.respondWith(updateCache(request));
+// });
 
-async function precache() {
-  const cache = await caches.open(VERSION);
-  return cache.addAll([
-    '/',
-    'index.html',
-    'assets/js/index.js',
-    'assets/js/mediaPlayer.js',
-    'assets/plugins/AutoPlay.js',
-    'assets/plugins/AutoPause.js',
-    'assets/video/BigBuckBunny.mp4',
-    'css/styles.css',
-  ]);
-}
+// async function precache() {
+//   const cache = await caches.open(VERSION);
+//   return cache.addAll([
+//     '/',
+//     'index.html',
+//     'assets/js/index.js',
+//     'assets/js/mediaPlayer.js',
+//     'assets/plugins/AutoPlay.js',
+//     'assets/plugins/AutoPause.js',
+//     'assets/video/BigBuckBunny.mp4',
+//     'css/styles.css',
+//   ]);
+// }
 
-async function cacheResponse(request) {
-  const cache = await caches.open(VERSION);
-  const response = await cache.match(request);
-  return response || fetch(request);
-}
+// async function cacheResponse(request) {
+//   const cache = await caches.open(VERSION);
+//   const response = await cache.match(request);
+//   return response || fetch(request);
+// }
 
-async function updateCache(request) {
-  const cache = await caches.open(VERSION);
-  const response = await fetch(request);
-  return cache.put(request, response);
-}
+// async function updateCache(request) {
+//   const cache = await caches.open(VERSION);
+//   const response = await fetch(request);
+//   return cache.put(request, response);
+// }
