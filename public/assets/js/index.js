e00ab1463090f0fc2a365f93f2d71a6a3f98d60e
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-05-03 15:07:48 -0400

    Service worker y otros avences

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,21 @@
 import MediaPlayer from './mediaPlayer.js';
 import AutoPlay from '../plugins/AutoPlay.js';
+import AutoPause from '../plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const buttonPlay = document.querySelector('#play');
 const buttonMute = document.querySelector('#mute');
 
-const player = new MediaPlayer({el: video, plugins: [ 
-  // new AutoPlay()
+const player = new MediaPlayer({el: video, plugins: [
+  new AutoPlay(),
+  new AutoPause()
 ]});
+
 buttonPlay.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
 // button.onclick = () => video.pause();
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => { console.log(error.message) });
+}
--- public/assets/plugins/AutoPlay.js
+++ public/assets/plugins/AutoPlay.js
@@ -1,7 +1,9 @@
 function AutoPlay() {}
 
 AutoPlay.prototype.run = function (player) {
-  player.mute();
+  if (!player.muted) {
+    player.mute(true);
+  }
   player.play();
 }
 
